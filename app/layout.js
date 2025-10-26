38c542d65cb608094ec9df082a61981c67499660
Naren7874 <malinarendra7874@gmail.com>
2025-10-27 03:57:55 +0530

    add user budget alert email

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
           <Header/>
           <main className="min-h-screen">{children}</main>
           <Toaster richColors />
-          <footer className="bg-blue-50 py-12">
+          <footer className="bg-blue-50 py-5">
             <div className="container mx-auto px-4 text-center text-gray-600">
               <p>Made by Narendra Mali</p>
             </div>
